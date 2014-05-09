6c5b53fcea89a153c1d18480bee47da9460c7728
Peter IJlst <peter@bureaublauwgeel.nl>
2014-05-09 21:35:21 +0200

    minor fix in event handling

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -1,12 +1,15 @@
-$('.navbar-form').submit(function(e){
-  e.preventDefault();
-  var url   = '/search/';
-  var query = $('.form-control').val();
-  if(url.length > 0 && query.length > 0){
-    document.location = url + query;
-  }
-});
+$(function() {
+  $('.navbar-form').on('submit',function(e){
+    e.preventDefault();
+    var url   = '/search/';
+    var query = $('.form-control').val();
+    if(url.length > 0 && query.length > 0){
+      document.location = url + query;
+    }
+  });
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=0;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-50821313-1', 't0rrents.herokuapp.com');
-ga('send', 'pageview');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=0;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-50821313-1', 't0rrents.herokuapp.com');
+  ga('send', 'pageview');
+
+});
